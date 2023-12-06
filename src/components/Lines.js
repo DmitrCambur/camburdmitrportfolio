90104510e7f9495ed54584d52e85383cfcbd5fe4
Dmitr <dmitr.cambur@student.ehb.be>
2023-12-06 10:22:17 +0100

    updated lines.js

--- src/components/Lines.js
+++ src/components/Lines.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 
 const Lines = () => {
     const ref = useRef();
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
     useEffect(() => {
-        const drawMountains = (width, height) => {
+        // Function to draw the mountain lines
+        const drawMountains = () => {
             // Clear any existing content
             d3.select(ref.current).selectAll("svg").remove();
 
+            const { width, height } = dimensions;
             const svg = d3.select(ref.current)
                 .append("svg")
                 .attr("width", width)
@@ -17,20 +20,17 @@ const Lines = () => {
 
             // Define the number of lines and the maximum mountain height
             const numberOfLines = 20;
-            const maxMountainHeight = height / (1 * numberOfLines); // Reduced height
-            const peakDistance = width / 3; // The distance between peaks
-            const lineMargin = maxMountainHeight / 100; // Margin between lines
+            const maxMountainHeight = height / 2; // Adjust the maximum mountain height
+            const lineMargin = 10; // Margin between lines
 
             // Generate mountain line data
             const mountains = Array.from({ length: numberOfLines }, (_, i) => {
-                const peakHeight = (numberOfLines - i) * (maxMountainHeight - lineMargin) / numberOfLines;
-                const baseLineY = height - i * (maxMountainHeight - lineMargin / 2);
+                const peakHeight = Math.random() * maxMountainHeight;
+                const baseLineY = height - i * lineMargin;
                 return [
-                    { x: 0, y: baseLineY }, // start at left
-                    { x: width / 3 - peakDistance / 2, y: baseLineY }, // before peak
-                    { x: width / 3, y: baseLineY - peakHeight }, // peak
-                    { x: width / 3 + peakDistance / 2, y: baseLineY }, // after peak
-                    { x: width, y: baseLineY } // end at right
+                    { x: 0, y: baseLineY },
+                    { x: Math.random() * width, y: baseLineY - peakHeight },
+                    { x: width, y: baseLineY }
                 ];
             });
 
@@ -43,23 +43,47 @@ const Lines = () => {
                 .attr("stroke", "black")
                 .attr("stroke-width", 1)
                 .attr("stroke-linecap", "round");
+
+            // Event listener for mouse movement to adjust the peak
+            svg.on("mousemove", function(event) {
+                const mousePosition = d3.pointer(event);
+                const mouseX = mousePosition[0];
+
+                // Update the lines based on mouse X position
+                svg.selectAll("path").data(mountains).attr("d", d => {
+                    const middleIndex = Math.floor(d.length / 2);
+                    d[middleIndex].x = mouseX;
+                    return d3.line().x(d => d.x).y(d => d.y)(d);
+                });
+            });
         };
 
-        // Set up a resize observer to redraw the chart when the container size changes
+        // Set up a resize observer to update dimensions
         const resizeObserver = new ResizeObserver(entries => {
             if (!entries || entries.length === 0) {
                 return;
             }
-            const { width, height } = entries[0].contentRect;
-            drawMountains(width, height);
+            setDimensions({
+                width: entries[0].contentRect.width,
+                height: entries[0].contentRect.height
+            });
         });
 
-        // Observe the ref element for size changes
+        // Start observing for resize
         resizeObserver.observe(ref.current);
 
-        // Clean up the observer on component unmount
-        return () => resizeObserver.disconnect();
+        // Clean up on unmount
+        return () => {
+            resizeObserver.disconnect();
+        };
     }, []);
+
+    // Redraw the mountains whenever the dimensions change
+    useEffect(() => {
+        if (dimensions.width && dimensions.height) {
+            drawMountains();
+        }
+    }, [dimensions]);
 
     return <div ref={ref} className="lines-container" style={{ width: '100%', height: '100%' }} />;
 };
